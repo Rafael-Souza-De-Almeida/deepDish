1a4efa698f2410a5b482abf2b9e6c89fdd882490
Rafael-Souza-De-Almeida <rafaelalmeida37@ufrrj.br>
2026-01-16 21:38:37 -0300

    feat: integrate Tailwind CSS and Radix UI components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./app";
+import "./index.css";
+import "./globals.css";
+import { ThemeProvider } from "./components/theme_provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello, World!</h1>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
